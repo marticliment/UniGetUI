0790df76e277e8a970ac2dd1286ab63330cbbbea
Martí Climent <marticlilop@gmail.com>
2022-10-03 18:37:37 +0200

    Fix scoop installer

--- wingetui/resources/install_scoop.cmd
+++ wingetui/resources/install_scoop.cmd
@@ -1,9 +1,9 @@
 @echo off
-echo This script is going to install scoop and it's required dependencies required by WingetUI. Press any key to continue
+echo This script is going to install scoop and it's required dependencies required by WingetUI.
 pause
 powershell -NoProfile -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
 echo Installing scoop...
-powershell -NoProfile -Command 'iex "& {$(irm get.scoop.sh)} -RunAsAdmin"'
+powershell -NoProfile -Command "iex ""& {$(irm get.scoop.sh)} -RunAsAdmin"""
 echo Installing git...
 powershell -NoProfile -Command "scoop install git"
 echo Done!
