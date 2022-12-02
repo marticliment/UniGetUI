c642d4ff5f5dc61789cc0f1acdcc8f5d1780b4c7
ppvnf <73800734+ppvnf@users.noreply.github.com>
2022-12-02 07:55:53 -0300

    fix typo

--- wingetui/resources/uninstall_scoop.cmd
+++ wingetui/resources/uninstall_scoop.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo This script will remove scoop from your machine.
-echo Removing scoop implies removing all scoop installed packages, buckets and preferences, and might also erase vaulable user data related to the affected packages
+echo Removing scoop implies removing all scoop installed packages, buckets and preferences, and might also erase valuable user data related to the affected packages
 echo|set/p="Press <ENTER> to continue or CLOSE this window to abort this process"&runas/u: "">NUL
 echo Installing scoop...
 powershell -NoProfile -Command "scoop uninstall -p scoop"
