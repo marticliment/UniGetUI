7beb96137d103e891672ae97f6039736ac7ee0e7
Martí Climent <marticlilop@gmail.com>
2022-12-16 20:49:09 +0100

    Reload environment variables before attempting to install scoop (#495)

--- wingetui/resources/install_scoop.ps1
+++ wingetui/resources/install_scoop.ps1
@@ -2,6 +2,8 @@ Write-Output "Installing scoop..."
 
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
+$Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")  
+
 If (-Not (Get-Command git -ErrorAction SilentlyContinue)) {
     Write-Output "Installing git..."
     scoop install git
