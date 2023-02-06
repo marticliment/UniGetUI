faa248ff8f63b3706307c7ce5dc95ceaab75190d
Martí Climent <marticlilop@gmail.com>
2023-02-06 12:49:27 +0100

    add a new utility to detect old, outdated translations

---
+++ scripts/purge_unusedtranslations.py
@@ -0,0 +1,26 @@
+import os, glob, json
+
+try:
+    os.chdir("../wingetui/lang/")
+
+    contents = ""
+
+    with open("lang_en.json", "r") as f:
+        engfile = json.load(f)
+
+    os.chdir("../../")
+    print(f"Working on 📂 {os.getcwd()}")
+
+    for codefile in glob.glob("**/*.py", recursive=True):
+        print(f"Reading 📄 {codefile}")
+        with open(codefile, "r", errors="ignore") as f:
+            contents += f.read()
+        contents += " ################################ File division #########################################"
+    for key in engfile.keys():
+        if not key in contents:
+            print("Unused key 😳: "+str(key))
+    print("Job finished succuessfully! 😎")
+except Exception as e:
+    print("FAILED:", e)
+        
+os.system("pause")
