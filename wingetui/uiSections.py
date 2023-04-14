f064674977d4eafc392a550a0ace275c604be049
Martí Climent <marticlilop@gmail.com>
2023-04-14 23:19:25 +0200

    remove incorrect scoop entries from the updates (packages missing manifest)

--- wingetui/uiSections.py
+++ wingetui/uiSections.py
@@ -1311,7 +1311,7 @@ class UpdateSoftwareSection(QWidget):
         self.callInMain.emit(partial(item.setText, 5, store))
 
     def addItem(self, name: str, id: str, version: str, newVersion: str, store) -> None:
-        if not "---" in name and not "The following packages" in name and not "Name  " in name and not name in ("+", "Scoop", "At", "The", "But") and not version in ("the", "is"):
+        if not "---" in name and not "The following packages" in name and not "Name  " in name and not name in ("+", "Scoop", "At", "The", "But") and not version in ("the", "is") and not newVersion in ("Manifest", version):
             if not id in self.blacklist:
                 item = TreeWidgetItemWithQAction()
                 item.setText(1, name)
