24e6493828a8e6358d406961ba6c0badc75aac9f
Martí Climent <marticlilop@gmail.com>
2023-04-15 00:54:38 +0200

    remoge scoop ghost package

--- wingetui/uiSections.py
+++ wingetui/uiSections.py
@@ -586,7 +586,7 @@ class DiscoverSoftwareSection(QWidget):
         return super().resizeEvent(event)
     
     def addItem(self, name: str, id: str, version: str, store) -> None:
-        if not "---" in name and not name in ("+", "Scoop", "At", "The", "But") and not version in ("the", "is"):
+        if not "---" in name and not name in ("+", "Scoop", "At", "The", "But", "Au") and not version in ("the", "is"):
             item = TreeWidgetItemWithQAction(self)
             item.setText(1, name)
             item.setText(2, id)
@@ -1312,7 +1312,7 @@ class UpdateSoftwareSection(QWidget):
         self.callInMain.emit(partial(item.setText, 5, store))
 
     def addItem(self, name: str, id: str, version: str, newVersion: str, store) -> None:
-        if not "---" in name and not "The following packages" in name and not "Name  " in name and not name in ("+", "Scoop", "At", "The", "But") and not version in ("the", "is") and not newVersion in ("Manifest", version):
+        if not "---" in name and not "The following packages" in name and not "Name  " in name and not name in ("+", "Scoop", "At", "The", "But", "Au") and not version in ("the", "is") and not newVersion in ("Manifest", version):
             if not id in self.blacklist:
                 item = TreeWidgetItemWithQAction()
                 item.setText(1, name)
@@ -2094,7 +2094,7 @@ class UninstallSoftwareSection(QWidget):
 
 
     def addItem(self, name: str, id: str, version: str, store: str) -> None:
-        if not "---" in name and not name in ("+", "Scoop", "At", "The", "But") and not version in ("the", "is"):
+        if not "---" in name and not name in ("+", "Scoop", "At", "The", "But", "Au") and not version in ("the", "is"):
             item = TreeWidgetItemWithQAction()
             if store.lower() == "winget":
                 for illegal_char in ("{", "}", " "):
