54cd78f6a01ea088eabff10b09762cf2231e02d8
Martí Climent <marticlilop@gmail.com>
2023-04-28 21:11:52 +0200

    Winget packages will be cached, so load times will be faster

--- wingetui/PackageManagers/wingetHelpers.py
+++ wingetui/PackageManagers/wingetHelpers.py
@@ -11,76 +11,125 @@ if getSettings("UseSystemWinget"):
 else:
     winget = os.path.join(os.path.join(realpath, "winget-cli"), "winget.exe")
 
-def searchForPackage(signal: Signal, finishSignal: Signal, noretry: bool = False) -> None:
-    print(f"🟢 Starting winget search, winget on {winget}...")
-    p = subprocess.Popen(["mode", "400,30&", winget, "search", ""] + common_params ,stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, cwd=os.getcwd(), env=os.environ.copy(), shell=True)
-    output = []
-    counter = 0
-    idSeparator = 0
-    while p.poll() is None:
-        line = p.stdout.readline()
-        line = line.strip()
-        if line:
-            if(counter > 0):
-                output.append(str(line, encoding='utf-8', errors="ignore"))
-            else:
-                l = str(line, encoding='utf-8', errors="ignore").replace("\x08-\x08\\\x08|\x08 \r","")
-                l = l.split("\r")[-1]
-                if("Id" in l):
-                    idSeparator = len(l.split("Id")[0])
-                    verSeparator = idSeparator+2
-                    i=0
-                    while l.split("Id")[1].split(" ")[i] == "":
-                        verSeparator += 1
-                        i += 1
-                    counter += 1
-    print(p.stderr)
-    if p.returncode != 0 and not noretry:
-        time.sleep(1)
-        print(p.returncode)
-        searchForPackage(signal, finishSignal, noretry=True)
-    else:
-        counter = 0
-        for element in output:
+def processElement(element: str, idSeparator: int, verSeparator: int) -> tuple[str]:
+    """
+    Will return a tuple made out of 4 strings.
+    """
+    try:
+        verElement = element[idSeparator:].strip()
+        verElement.replace("\t", " ")
+        while "  " in verElement:
+            verElement = verElement.replace("  ", " ")
+        iOffset = 0
+        id = verElement.split(" ")[iOffset+0]
+        try:
+            ver = verElement.split(" ")[iOffset+1]
+        except IndexError:
+            ver = _("Unknown")
+        if len(id)==1:
+            iOffset + 1
+            id = verElement.split(" ")[iOffset+0]
             try:
-                verElement = element[idSeparator:].strip()
-                verElement.replace("\t", " ")
-                while "  " in verElement:
-                    verElement = verElement.replace("  ", " ")
-                iOffset = 0
-                id = verElement.split(" ")[iOffset+0]
-                try:
-                    ver = verElement.split(" ")[iOffset+1]
-                except IndexError:
-                    ver = _("Unknown")
-                if len(id)==1:
-                    iOffset + 1
-                    id = verElement.split(" ")[iOffset+0]
-                    try:
-                        ver = verElement.split(" ")[iOffset+1]
-                    except IndexError:
-                        ver = "Unknown"
-                if ver.strip() in ("<", "-", ""):
-                    iOffset += 1
-                    ver = verElement.split(" ")[iOffset+1]
-                if not "  " in element[0:idSeparator].strip():
-                    signal.emit(element[0:idSeparator].strip(), id, ver, "Winget")
-                else:
-                    element = bytes(element, "utf-8")
-                    export = (element[0:idSeparator], str(element[idSeparator:], "utf-8").strip().split(" ")[0], list(filter(None, str(element[idSeparator:], "utf-8").strip().split(" ")))[1])
-                    signal.emit(str(export[0], "utf-8").strip(), export[1], export[2], "Winget")
+                ver = verElement.split(" ")[iOffset+1]
+            except IndexError:
+                ver = "Unknown"
+        if ver.strip() in ("<", "-", ""):
+            iOffset += 1
+            ver = verElement.split(" ")[iOffset+1]
+        if not "  " in element[0:idSeparator].strip():
+            return (element[0:idSeparator].strip(), id, ver, "Winget")
+        else:
+            element = bytes(element, "utf-8")
+            export = (element[0:idSeparator], str(element[idSeparator:], "utf-8").strip().split(" ")[0], list(filter(None, str(element[idSeparator:], "utf-8").strip().split(" ")))[1])
+            return (str(export[0], "utf-8").strip(), export[1], export[2], "Winget")
+    except Exception as e:
+        try:
+            report(e)
+            try:
+                element = str(element, "utf-8")
             except Exception as e:
-                try:
-                    report(e)
-                    try:
-                        element = str(element, "utf-8")
-                    except Exception as e:
-                        print(e)
-                    signal.emit(element[0:idSeparator].strip(), element[idSeparator:verSeparator].strip(), element[verSeparator:].split(" ")[0].strip(), "Winget")
-                except Exception as e:
-                    report(e)
-        print("🟢 Winget search finished")
-        finishSignal.emit("winget")  # type: ignore
+                print(e)
+            return (element[0:idSeparator].strip(), element[idSeparator:verSeparator].strip(), element[verSeparator:].split(" ")[0].strip(), "Winget")
+        except Exception as e:
+            report(e)
+    return ("", "", "", "")
+   
+def searchForPackage(signal: Signal, finishSignal: Signal, noretry: bool = False) -> None:
+    try:
+        print("🔵 Starting winget search")
+        cacheFile = os.path.join(os.path.expanduser("~"), ".wingetui/cacheddata/wingetpackages")
+        cachePath = os.path.dirname(cacheFile)
+        correctCache = False
+        NeedToCacheIdSeparator = True
+        if not os.path.exists(cachePath):
+            os.makedirs(cachePath)
+        if os.path.exists(cacheFile):
+            with open(cacheFile, "r", encoding='utf-8', errors="ignore") as f:
+                content = f.read()
+                if content != "":
+                    print("🟢 Found valid cache for winget!")
+                    for line in content.split("\n"):
+                        if NeedToCacheIdSeparator:
+                            line = line.split("\r")[-1]
+                            if("Id" in line):
+                                idSeparator = len(line.split("Id")[0])
+                                verSeparator = idSeparator+2
+                                i=0
+                                while line.split("Id")[1].split(" ")[i] == "":
+                                    verSeparator += 1
+                                    i += 1
+                                NeedToCacheIdSeparator = False
+                        else:
+                            r = processElement(line, idSeparator, verSeparator)
+                            if r[0]:
+                                signal.emit(r[0], r[1], r[2], r[3])
+                            correctCache = True
+                    if correctCache:
+                        finishSignal.emit("winget")
+            
+        print("🔵 Starting winget file update...")
+        p = subprocess.Popen(["mode", "400,30&", winget, "search", ""] + common_params ,stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, cwd=os.getcwd(), env=os.environ.copy(), shell=True)
+        output = ""
+        oldcontents = ""
+        NeedToCacheIdSeparator = True
+        while p.poll() is None:
+            line = p.stdout.readline()
+            line = line.strip()
+            if line:
+                l = str(line, encoding='utf-8', errors="ignore").replace("\x08-\x08\\\x08|\x08 \r","")
+                output += l +"\n"
+                if not correctCache:
+                    if NeedToCacheIdSeparator:
+                        l = l.split("\r")[-1]
+                        if("Id" in l):
+                            idSeparator = len(l.split("Id")[0])
+                            verSeparator = idSeparator+2
+                            i=0
+                            while l.split("Id")[1].split(" ")[i] == "":
+                                verSeparator += 1
+                                i += 1
+                            NeedToCacheIdSeparator = False
+                    else:
+                        r = processElement(l, idSeparator, verSeparator)
+                        if r[0]:
+                            signal.emit(r[0], r[1], r[2], r[3])
+
+        try:
+            with open(cacheFile, "r", encoding="utf-8", errors="ignore") as f:
+                oldcontents = f.read()
+                f.close()
+        except Exception as e:
+            report(e)
+        for line in oldcontents.split("\n"):
+            if line.split(" ")[0] not in output:
+                output += line + "\n"
+        with open(cacheFile, "w", encoding="utf-8", errors="ignore") as f:
+            f.write(output)
+        finishSignal.emit("winget")  
+        print("🟢 Winget cache rebuilt")
+    except Exception as e:
+        report(e)
+        finishSignal.emit("winget")  
 
 def searchForOnlyOnePackage(id: str) -> tuple[str, str]:
     print(f"🟢 Starting winget search, winget on {winget}...")
@@ -124,7 +173,6 @@ def searchForUpdates(signal: Signal, finishSignal: Signal, noretry: bool = False
                 l = str(line, encoding='utf-8', errors="ignore").replace("\x08-\x08\\\x08|\x08 \r","")
                 for char in ("\r", "/", "|", "\\", "-"):
                     l = l.split(char)[-1].strip()
-                print(l)
                 if("Id" in l):
                     idSeparator = len(l.split("Id")[0])
                     verSeparator = len(l.split("Version")[0])
@@ -283,13 +331,11 @@ def getInfo(signal: Signal, title: str, id: str, useId: bool, progId: str) -> No
             }
             while p.poll() is None:
                 line = p.stdout.readline()
-                cprint(line)
                 if line:
                     output.append(str(line, encoding='utf-8', errors="ignore"))
             weAreDescripting = False
             weAreReleaseNoting = False
             for line in output:
-                cprint(line)
                 if line[0] == " " and weAreDescripting:
                     packageDetails["description"] += "\n"+line
                 else:
@@ -364,8 +410,7 @@ def getInfo(signal: Signal, title: str, id: str, useId: bool, progId: str) -> No
                         output.append(str(line, encoding='utf-8', errors="ignore"))
                     else:
                         counter += 1
-            cprint("Output: ")
-            cprint(output)
+            print("Output: ")
         packageDetails["versions"] = output
         signal.emit(packageDetails, progId)
     except Exception as e:
