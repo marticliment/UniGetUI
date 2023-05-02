e03b02089927d25f8d9bd232b899a99b01f4fb26
Martí Climent <marticlilop@gmail.com>
2023-05-02 21:55:52 +0200

    add new class

--- wingetui/PackageManagers/PackageClasses.py
+++ wingetui/PackageManagers/PackageClasses.py
@@ -18,3 +18,33 @@ class UpgradablePackage(Package):
         super().__init__(Name, Id, InstalledVersion, Source)
         self.NewVersion = NewVersion
         self.NewPackage = Package(Name, Id, NewVersion, Source)
+        
+class PackageDetails(Package):
+    Name: str = ""
+    Id: str = ""
+    Version: str = ""
+    NewVersion: str = ""
+    Source: str = ""
+    Publisher: str = ""
+    Author: str = ""
+    Description: str = ""
+    HomepageURL: str = ""
+    License: str = ""
+    LicenseURL: str = ""
+    InstallerURL: str = ""
+    InstallerHash: str = ""
+    InstallerSize: int = 0
+    InstallerType: str = ""
+    ManifestUrl: str = ""
+    UpdateDate: str = ""
+    ReleaseNotes: str = ""
+    ReleaseNotesUrl: str = ""
+    Versions: list[str] = []
+    Architectures: list[str] = []
+    Scopes: list[str] = []
+    
+    def __init__(self, package: Package):
+        self.Name = package.Name
+        self.Id = package.Id
+        self.Version = package.Version
+        self.Source = package.Source
