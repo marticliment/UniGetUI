382e233cbe1abe1ea9b26e00900e3c6ad22b4c5e
Mike <Mikey1993@users.noreply.github.com>
2023-05-04 01:06:21 +0300

    Update apply_versions.py
    Use the `with` statement to open files

--- scripts/apply_versions.py
+++ scripts/apply_versions.py
@@ -4,28 +4,26 @@ import os
 os.chdir(os.path.join(os.path.dirname(__file__), "..")) # move to root project
 
 try:
-
     sys.path.append("wingetui")
 
     from versions import *
 
 
     def fileReplaceLinesWith(filename: str, list: dict[str, str], encoding="utf-8"):
-        f = open(filename, "r+", encoding=encoding, errors="ignore")
-        data = ""
-        for line in f.readlines():
-            match = False
-            for key, value in list.items():
-                if (line.startswith(key)):
-                    data += f"{key}{value}"
-                    match = True
-                    continue
-            if (not match):
-                data += line
-        f.seek(0)
-        f.write(data)
-        f.truncate()
-        f.close()
+        with open(filename, "r+", encoding=encoding, errors="ignore") as f:
+            data = ""
+            for line in f.readlines():
+                match = False
+                for key, value in list.items():
+                    if (line.startswith(key)):
+                        data += f"{key}{value}"
+                        match = True
+                        continue
+                if (not match):
+                    data += line
+            f.seek(0)
+            f.write(data)
+            f.truncate()
 
 
     fileReplaceLinesWith("WingetUI.iss", {
