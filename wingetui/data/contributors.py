4beff24c3ea8dc82a8dda83613a791183f903af5
Martí Climent <marticlilop@gmail.com>
2023-05-14 12:35:50 +0200

    update translations

--- wingetui/data/contributors.py
+++ wingetui/data/contributors.py
@@ -27,7 +27,7 @@ contributorsInfo = [
   {
     "name": "marticliment",
     "link": "https://github.com/marticliment",
-    "contributions": 1409
+    "contributions": 1410
   },
   {
     "name": "ppvnf",
