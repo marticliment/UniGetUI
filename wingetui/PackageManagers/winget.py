5ecd0454c371b5bbd3b181e1cd1159cd974994c8
Martí Climent <marticlilop@gmail.com>
2023-06-10 23:26:14 +0200

    Blacklist incorrect Winget packages (fix #1076, fix #1090)

--- wingetui/PackageManagers/winget.py
+++ wingetui/PackageManagers/winget.py
@@ -17,8 +17,8 @@ class WingetPackageManager(DynamicPackageManager):
     CACHE_FILE_PATH = os.path.join(os.path.expanduser("~"), ".wingetui/cacheddata")
 
     BLACKLISTED_PACKAGE_NAMES = [""]
-    BLACKLISTED_PACKAGE_IDS = ["", "have"]
-    BLACKLISTED_PACKAGE_VERSIONS = ["have", "an"]
+    BLACKLISTED_PACKAGE_IDS = ["", "have", "the", "Id"]
+    BLACKLISTED_PACKAGE_VERSIONS = ["have", "an", "'winget", "pin'", "have", "an", "Version"]
     
     Capabilities = PackageManagerCapabilities()
     Capabilities.CanRunAsAdmin = True
