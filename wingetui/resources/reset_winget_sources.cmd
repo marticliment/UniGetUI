798bbdce50380bc916f3cedc36bdb58d7de39986
Martí Climent <marticlilop@gmail.com>
2023-06-11 18:45:21 +0200

    Fix winget source reset script (fix #1074)

--- wingetui/resources/reset_winget_sources.cmd
+++ wingetui/resources/reset_winget_sources.cmd
@@ -1,15 +1,19 @@
 @echo off
+SET dirpath="%~dp0..\winget-cli\"
 SET wingetpath="%~dp0..\winget-cli\winget.exe"
-SET sudopath="%~dp0..\sudo\gsudo.exe"
+SET sudopath="%~dp0..\components\gsudo.exe"
 echo This script will reset winget sources.
 echo You might be asked for administrator rights up to three times during this process.
 echo|set/p="Press <ENTER> to continue or CLOSE THIS WINDOW to abort this process"&runas/u: "">NUL
 echo . Please wait... 
 echo winget path: %wingetpath%
 echo Performing reset...
+cd %dirpath%
+%sudopath% cache on
 %sudopath% %wingetpath% source remove winget
 %sudopath% %wingetpath% source add winget https://cdn.winget.microsoft.com/cache
 %sudopath% %wingetpath% source reset --force
+%sudopath% cache off
 echo Task completed!
 echo You can now close this window and refresh your wingetui listings
 pause
