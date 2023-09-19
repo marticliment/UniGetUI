7d687f00ec70c8e31325588d3e9fc7eff2251b4a
Martí Climent <marticlilop@gmail.com>
2023-09-19 19:46:19 +0200

    Update sampleHelper.py

--- wingetui/PackageManagers/sampleHelper.py
+++ wingetui/PackageManagers/sampleHelper.py
@@ -46,87 +46,43 @@ class SamplePackageManager(PackageManagerModule):
 
     def isEnabled(self) -> bool:
         return not getSettings(f"Disable{self.NAME}")
-
+    
     def getAvailablePackages(self, second_attempt: bool = False) -> list[Package]:
-        f"""
-        Will retieve the cached packages for the package manager {self.NAME} in the format of a list[Package] object.
-        If the cache is empty, will forcefully cache the packages and return a valid list[Package] object.
-        Finally, it will start a background cacher thread.
-        """
-        print(f"🔵 Starting {self.NAME} search for available packages")
-        try:
-            packages: list[Package] = []
-            if os.path.exists(self.CACHE_FILE):
-                f = open(self.CACHE_FILE, "r", encoding="utf-8", errors="ignore")
-                content = f.read()
-                f.close()
-                if content != "":
-                    print(f"🟢 Found valid, non-empty cache file for {self.NAME}!")
-                    for line in content.split("\n"):
-                        package = line.split(",")
-                        if len(package) >= 3 and not package[0] in self.BLACKLISTED_PACKAGE_NAMES and not package[1] in self.BLACKLISTED_PACKAGE_IDS and not package[2] in self.BLACKLISTED_PACKAGE_VERSIONS:
-                            packages.append(Package(formatPackageIdAsName(package[0]), package[1], package[2], self.NAME, self))
-                    Thread(target=self.cacheAvailablePackages, daemon=True, name=f"{self.NAME} package cacher thread").start()
-                    print(f"🟢 {self.NAME} search for installed packages finished with {len(packages)} result(s)")
-                    return packages
-                else:
-                    print(f"🟠 {self.NAME} cache file exists but is empty!")
-                    if second_attempt:
-                        print(f"🔴 Could not load {self.NAME} packages, returning an empty list!")
-                        return []
-                    self.cacheAvailablePackages()
-                    return self.getAvailablePackages(second_attempt=True)
-            else:
-                print(f"🟡 {self.NAME} cache file does not exist, creating cache forcefully and returning new package list")
-                if second_attempt:
-                    print(f"🔴 Could not load {self.NAME} packages, returning an empty list!")
-                    return []
-                self.cacheAvailablePackages()
-                return self.getAvailablePackages(second_attempt=True)
-        except Exception as e:
-            report(e)
-            return []
+        print(f"🟠 Package manager {self.NAME} does not support listing available packages")
+        return []
 
     def cacheAvailablePackages(self) -> None:
+        print(f"🟠 Package manager {self.NAME} does not support caching available packages")
+
+    def getPackagesForQuery(self, query: str) -> list[Package]:
+        f"""
+        Will retieve the packages for the given "query: str" from the package manager {self.NAME} in the format of a list[Package] object.
         """
-        INTERNAL METHOD
-        Will load the available packages and write them into the cache file
-        """
-        print(f"🔵 Starting {self.NAME} package caching")
+        print(f"🔵 Starting {self.NAME} query search")
         try:
-            p = subprocess.Popen([self.NAME, "search", "*"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, shell=True)
-            ContentsToCache = ""
+            p = subprocess.Popen([self.EXECUTABLE, "search", "*"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, shell=True)
+            packages: list[Package] = []
             while p.poll() is None:
                 line: str = str(p.stdout.readline().strip(), "utf-8", errors="ignore")
                 if line:
 
                     if len(line.split("|")) >= 3:
                         # Replace these lines with the parse mechanism
-                        self.NAME = formatPackageIdAsName(line.split("|")[0])
+                        name = formatPackageIdAsName(line.split("|")[0])
                         id = line.split("|")[0]
                         version = line.split("|")[1]
                         source = self.NAME
                     else:
                         continue
 
-                    if self.NAME not in self.BLACKLISTED_PACKAGE_NAMES and id not in self.BLACKLISTED_PACKAGE_IDS and version not in self.BLACKLISTED_PACKAGE_VERSIONS:
-                        ContentsToCache += f"{self.NAME},{id},{version},{source}\n"
+                    if name not in self.BLACKLISTED_PACKAGE_NAMES and id not in self.BLACKLISTED_PACKAGE_IDS and version not in self.BLACKLISTED_PACKAGE_VERSIONS:
+                        packages.append(Package(name, id, version, source, None))
 
-            AlreadyCachedPackages = ""
-            try:
-                with open(self.CACHE_FILE, "r") as f:
-                    AlreadyCachedPackages = f.read()
-                    f.close()
-            except Exception as e:
-                report(e)
-            for line in AlreadyCachedPackages.split("\n"):
-                if line.split(",")[0] not in ContentsToCache:
-                    ContentsToCache += line + "\n"
-            with open(self.CACHE_FILE, "w") as f:
-                f.write(ContentsToCache)
-            print(f"🟢 {self.NAME} packages cached successfuly")
+            print(f"🟢 {self.NAME} package query finished successfully")
+            return packages
         except Exception as e:
             report(e)
+            return []
 
     def getAvailableUpdates(self) -> list[UpgradablePackage]:
         f"""
@@ -144,7 +100,7 @@ class SamplePackageManager(PackageManagerModule):
 
                     if len(line.split("|")) >= 3:
                         # Replace these lines with the parse mechanism
-                        self.NAME = formatPackageIdAsName(line.split("|")[0])
+                        name = formatPackageIdAsName(line.split("|")[0])
                         id = line.split("|")[0]
                         version = line.split("|")[1]
                         newVersion = line.split("|")[2]
@@ -152,8 +108,8 @@ class SamplePackageManager(PackageManagerModule):
                     else:
                         continue
 
-                    if self.NAME not in self.BLACKLISTED_PACKAGE_NAMES and id not in self.BLACKLISTED_PACKAGE_IDS and version not in self.BLACKLISTED_PACKAGE_VERSIONS:
-                        packages.append(UpgradablePackage(self.NAME, id, version, newVersion, source, self))
+                    if name not in self.BLACKLISTED_PACKAGE_NAMES and id not in self.BLACKLISTED_PACKAGE_IDS and version not in self.BLACKLISTED_PACKAGE_VERSIONS:
+                        packages.append(UpgradablePackage(name, id, version, newVersion, source, self))
             print(f"🟢 {self.NAME} search for updates finished with {len(packages)} result(s)")
             globals.PackageManagerOutput += rawoutput
             return packages
@@ -177,15 +133,15 @@ class SamplePackageManager(PackageManagerModule):
 
                     if len(line.split("|")) >= 3:
                         # Replace these lines with the parse mechanism
-                        self.NAME = formatPackageIdAsName(line.split("|")[0])
+                        name = formatPackageIdAsName(line.split("|")[0])
                         id = line.split("|")[0]
                         version = line.split("|")[1]
                         source = self.NAME
                     else:
                         continue
 
-                    if self.NAME not in self.BLACKLISTED_PACKAGE_NAMES and id not in self.BLACKLISTED_PACKAGE_IDS and version not in self.BLACKLISTED_PACKAGE_VERSIONS:
-                        packages.append(Package(self.NAME, id, version, source, self))
+                    if name not in self.BLACKLISTED_PACKAGE_NAMES and id not in self.BLACKLISTED_PACKAGE_IDS and version not in self.BLACKLISTED_PACKAGE_VERSIONS:
+                        packages.append(Package(name, id, version, source, self))
             print(f"🟢 {self.NAME} search for installed packages finished with {len(packages)} result(s)")
             globals.PackageManagerOutput += rawoutput
             return packages
