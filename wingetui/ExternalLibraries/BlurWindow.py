dae4331f85a1b9850dfe5630c8eb0ae51bbb150a
Martí Climent <marticlilop@gmail.com>
2023-09-19 18:17:44 +0200

    lint BlurWindow with flake8

--- wingetui/ExternalLibraries/BlurWindow.py
+++ wingetui/ExternalLibraries/BlurWindow.py
@@ -3,7 +3,7 @@
 ExternalLibraries/BlurWindow.py
 
 This modified library applies blur effect to menus.
-Code sources: 
+Code sources:
  - https://pypi.org/project/BlurWindow/
  - https://github.com/Opticos/GWSL-Source/blob/master/blur.py
  - https://www.cnblogs.com/zhiyiYo/p/14659981.html
@@ -21,7 +21,6 @@ if platform.system() == 'Windows':
     user32 = ctypes.windll.user32
     dwm = ctypes.windll.dwmapi
 
-
     class ACCENTPOLICY(ctypes.Structure):
         _fields_ = [
             ("AccentState", ctypes.c_uint),
@@ -30,14 +29,12 @@ if platform.system() == 'Windows':
             ("AnimationId", ctypes.c_uint)
         ]
 
-
     class WINDOWCOMPOSITIONATTRIBDATA(ctypes.Structure):
         _fields_ = [
             ("Attribute", ctypes.c_int),
             ("Data", ctypes.POINTER(ctypes.c_int)),
             ("SizeOfData", ctypes.c_size_t)
         ]
-
 
     class DWM_BLURBEHIND(ctypes.Structure):
         _fields_ = [
@@ -47,7 +44,6 @@ if platform.system() == 'Windows':
             ('fTransitionOnMaximized', BOOL)
         ]
 
-
     class MARGINS(ctypes.Structure):
         _fields_ = [("cxLeftWidth", ctypes.c_int),
                     ("cxRightWidth", ctypes.c_int),
@@ -55,8 +51,8 @@ if platform.system() == 'Windows':
                     ("cyBottomHeight", ctypes.c_int)
                     ]
 
-
     SetWindowCompositionAttribute = user32.SetWindowCompositionAttribute
+
 
 def ExtendFrameIntoClientArea(hwnd):
 
@@ -75,7 +71,8 @@ def ExtendFrameIntoClientArea(hwnd):
     m.cyBottomHeight = -1
     return DwmExtendFrameIntoClientArea(hwnd, m)
 
-def HEXtoRGBAint(HEX:str):
+
+def HEXtoRGBAint(HEX: str):
     alpha = HEX[7:]
     blue = HEX[5:7]
     green = HEX[3:5]
@@ -87,39 +84,40 @@ def HEXtoRGBAint(HEX:str):
 
 def ApplyBlur(hwnd, hexColor=False, Acrylic=False, Dark=False, smallCorners=False):
     accent = ACCENTPOLICY()
-    accent.AccentState = 3 #Default window Blur #ACCENT_ENABLE_BLURBEHIND
+    accent.AccentState = 3  # Default window Blur #ACCENT_ENABLE_BLURBEHIND
 
     gradientColor = 0
 
-    if hexColor != False:
+    if hexColor is not False:
         gradientColor = HEXtoRGBAint(hexColor)
-        accent.AccentFlags = 2 #Window Blur With Accent Color #ACCENT_ENABLE_TRANSPARENTGRADIENT
+        accent.AccentFlags = 2  # Window Blur With Accent Color #ACCENT_ENABLE_TRANSPARENTGRADIENT
 
     if Acrylic:
-        accent.AccentState = 4 #UWP but LAG #ACCENT_ENABLE_ACRYLICBLURBEHIND
-        if hexColor == False: #UWP without color is translucent
+        accent.AccentState = 4  # UWP but LAG #ACCENT_ENABLE_ACRYLICBLURBEHIND
+        if hexColor is not False:  # UWP without color is translucent
             accent.AccentFlags = 2
-            gradientColor = HEXtoRGBAint('#12121240') #placeholder color
+            gradientColor = HEXtoRGBAint('#12121240')  # placeholder color
 
     accent.GradientColor = gradientColor
 
     data = WINDOWCOMPOSITIONATTRIBDATA()
-    data.Attribute = 19 #WCA_ACCENT_POLICY
+    data.Attribute = 19  # WCA_ACCENT_POLICY
     data.SizeOfData = ctypes.sizeof(accent)
-    data.Data = ctypes.cast(ctypes.pointer(accent), ctypes.POINTER(ctypes.c_int))
+    data.Data = ctypes.cast(ctypes.pointer(
+        accent), ctypes.POINTER(ctypes.c_int))
 
     SetWindowCompositionAttribute(int(hwnd), data)
 
     if Dark:
-        data.Attribute = 26 #WCA_USEDARKMODECOLORS
+        data.Attribute = 26  # WCA_USEDARKMODECOLORS
 
         SetWindowCompositionAttribute(int(hwnd), data)
 
+    # Add rounded borders (My addition)
+    DwmSetWindowAttribute = dwm.DwmSetWindowAttribute
+    DwmSetWindowAttribute(int(hwnd), 33, ctypes.byref(ctypes.c_int(
+        3 if smallCorners else 2)), ctypes.sizeof(ctypes.c_int))  # Add rounded borders (My addition)
 
-    DwmSetWindowAttribute = dwm.DwmSetWindowAttribute #  Add rounded borders (My addition)
-    DwmSetWindowAttribute(int(hwnd), 33, ctypes.byref(ctypes.c_int(3 if smallCorners else 2)), ctypes.sizeof(ctypes.c_int)) # Add rounded borders (My addition)
 
-
-
-def GlobalBlur(HWND,hexColor=False,Acrylic=False,Dark=False,QWidget=None, smallCorners=False):
-    ApplyBlur(HWND,hexColor,Acrylic,Dark, smallCorners=smallCorners)
+def GlobalBlur(HWND, hexColor=False, Acrylic=False, Dark=False, QWidget=None, smallCorners=False):
+    ApplyBlur(HWND, hexColor, Acrylic, Dark, smallCorners=smallCorners)
