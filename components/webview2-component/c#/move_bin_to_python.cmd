58d3752de017925d3acdb5d775b1792202fdd2a1
Martí Climent <marticlilop@gmail.com>
2023-11-27 12:16:52 +0100

    update executable files to fix getUrl() method

--- components/webview2-component/c#/move_bin_to_python.cmd
+++ components/webview2-component/c#/move_bin_to_python.cmd
@@ -1,7 +1,3 @@
 pushd bin\x64\Release
-rmdir /Q /S WinFormsWebView.exe.WebView2
-popd
-rmdir /Q /S "..\Python Module\Component"
-mkdir "..\Python Module\Component"
-xcopy /S bin\x64\Release\* "..\src\pyside6webview2\Component"
+copy .\* ..\..\..\..\PyWebView2\lib
 pause
