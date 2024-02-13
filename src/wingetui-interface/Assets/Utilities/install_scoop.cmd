a6f818e8b687bbbaffe38b327acff543d4b36d72
Martí Climent <marticlilop@gmail.com>
2024-02-13 17:48:23 +0100

    Update scoop scripts

--- src/wingetui-interface/Assets/Utilities/install_scoop.cmd
+++ src/wingetui-interface/Assets/Utilities/install_scoop.cmd
@@ -3,7 +3,8 @@ SET wingetuipath=%~dp0..\wingetui.exe
 set pwsh=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
 echo Scoop Installer Assistant - WingetUI
 echo This script will install Scoop and its dependencies, since it appears that they are not installed on your machine.
-pause
+echo|set/p="Press <ENTER> to continue or CLOSE this window to abort this process"&runas/u: "">NUL
+cls
 %pwsh% Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 %pwsh% -ExecutionPolicy ByPass -File "%~dp0\install_scoop.ps1"
 if %errorlevel% equ 0 (
--- src/wingetui-interface/Assets/Utilities/scoop_cleanup.cmd
+++ src/wingetui-interface/Assets/Utilities/scoop_cleanup.cmd
@@ -1,2 +1,11 @@
+@echo off
+echo This script will clean the Scoop cache and run the cleanup command.
+echo|set/p="Press <ENTER> to continue or CLOSE this window to abort this process"&runas/u: "">NUL
+cls
+echo Cleaning Scoop cache...
 scoop cleanup --all --global
+scoop cache rm --all --global
+scoop cleanup --all
+scoop cache rm --all
+echo Done!
 pause
