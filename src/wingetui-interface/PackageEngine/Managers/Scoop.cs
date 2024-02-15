98ab6094547b8acecdae455591cc64c2e12b16b9
Martí Climent <marticlilop@gmail.com>
2024-02-15 17:54:55 +0100

    Add scoop logic to load package details

--- src/wingetui-interface/PackageEngine/Managers/Scoop.cs
+++ src/wingetui-interface/PackageEngine/Managers/Scoop.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -221,9 +224,131 @@ public class Scoop : PackageManagerWithSources
         return new ManagerSource(this, "main", new Uri("https://github.com/ScoopInstaller/Main"), 0, "");
     }
 
-    public override Task<PackageDetails> GetPackageDetails_UnSafe(Package package)
+    public override async Task<PackageDetails> GetPackageDetails_UnSafe(Package package)
     {
-        throw new NotImplementedException();
+        var details = new PackageDetails(package);
+        Process p = new Process();
+        p.StartInfo = new ProcessStartInfo()
+        {
+            FileName = Status.ExecutablePath,
+            Arguments = Properties.ExecutableCallArgs + " cat " + package.Source.Name + "/" + package.Id,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        p.Start();
+        string JsonString = await p.StandardOutput.ReadToEndAsync();
+
+        JsonObject RawInfo = JsonObject.Parse(JsonString) as JsonObject;
+
+        try
+        {
+            if (RawInfo.ContainsKey("description") && (RawInfo["description"] is JsonArray))
+            {
+                details.Description = "";
+                foreach (JsonNode note in RawInfo["description"] as JsonArray)
+                    details.Description += note.ToString() + "\n";
+                details.Description = details.Description.Replace("\n\n", "\n").Trim();
+            }
+            else if (RawInfo.ContainsKey("description"))
+                details.Description = RawInfo["description"].ToString();
+        }
+        catch (Exception ex) { AppTools.Log("Can't load description: " + ex); }
+
+        try
+        {
+            if (RawInfo.ContainsKey("innosetup"))
+                details.InstallerType = "Inno Setup (" + bindings.Translate("extracted") + ")";
+            else 
+                details.InstallerType = bindings.Translate("Scoop package");
+        }
+        catch (Exception ex) { AppTools.Log("Can't load installer type: " + ex); }
+
+        try
+        {
+            if (RawInfo.ContainsKey("homepage"))
+            {
+                details.HomepageUrl = new Uri(RawInfo["homepage"].ToString());
+                if(details.HomepageUrl.ToString().Contains("https://github.com/"))
+                    details.Author = details.HomepageUrl.ToString().Replace("https://github.com/", "").Split("/")[0];
+                else
+                    details.Author = details.HomepageUrl.Host.Split(".")[^2];
+            }
+        }
+        catch (Exception ex) { AppTools.Log("Can't load homepage: " + ex); }
+
+        try
+        {
+            if (RawInfo.ContainsKey("notes") && (RawInfo["notes"] is JsonArray))
+            {
+                details.ReleaseNotes = "";
+                foreach (JsonNode note in RawInfo["notes"] as JsonArray)
+                    details.ReleaseNotes += note.ToString() + "\n";
+                details.ReleaseNotes = details.ReleaseNotes.Replace("\n\n", "\n").Trim();
+            }
+            else if (RawInfo.ContainsKey("notes"))
+                details.ReleaseNotes = RawInfo["notes"].ToString();
+        }
+        catch (Exception ex) { AppTools.Log("Can't load notes: " + ex); }
+
+        try
+        {
+            if (RawInfo.ContainsKey("license"))
+            {
+                if (RawInfo["license"] is not JsonValue)
+                {
+                    details.License = RawInfo["license"]["identifier"].ToString();
+                    details.LicenseUrl = new Uri(RawInfo["license"]["url"].ToString());
+                }
+                else
+                    details.License = RawInfo["license"].ToString();
+            }
+        }
+        catch (Exception ex) { AppTools.Log("Can't load license: " + ex); }
+
+        try
+        {
+            if (RawInfo.ContainsKey("url") && RawInfo.ContainsKey("hash"))
+            {
+                if (RawInfo["url"] is JsonArray)
+                    details.InstallerUrl = new Uri(RawInfo["url"][0].ToString());
+                else
+                    details.InstallerUrl = new Uri(RawInfo["url"].ToString());
+                
+                if (RawInfo["hash"] is JsonArray)
+                    details.InstallerHash = RawInfo["hash"][0].ToString();
+                else
+                    details.InstallerHash = RawInfo["hash"].ToString();
+            }
+            else if (RawInfo.ContainsKey("architecture"))
+            {
+                var FirstArch = (RawInfo["architecture"] as JsonObject).ElementAt(0).Key;
+                details.InstallerHash = RawInfo["architecture"][FirstArch]["hash"].ToString();
+                details.InstallerUrl = new Uri(RawInfo["architecture"][FirstArch]["url"].ToString());
+            }
+            
+            WebRequest req = HttpWebRequest.Create(details.InstallerUrl);
+            req.Method = "HEAD";
+            WebResponse resp = await req.GetResponseAsync();
+            long ContentLength = 0;
+            if (long.TryParse(resp.Headers.Get("Content-Length"), out ContentLength))
+            {
+                details.InstallerSize = ContentLength / 1048576;
+            }
+        }
+        catch (Exception ex) { AppTools.Log("Can't load installer URL: " + ex); }
+
+        try
+        {
+            if (RawInfo.ContainsKey("checkver") && RawInfo["checkver"] is JsonObject && (RawInfo["checkver"] as JsonObject).ContainsKey("url"))
+                details.ReleaseNotesUrl = new Uri(RawInfo["checkver"]["url"].ToString());
+        }
+        catch (Exception ex) { AppTools.Log("Can't load notes URL: " + ex); }
+
+        return details;
+
     }
 
     protected override async Task<ManagerSource[]> GetSources_UnSafe()
