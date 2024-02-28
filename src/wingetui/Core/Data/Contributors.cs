92f3c73fdb97eb992eda050e9e65a2d09a5fa4b0
Martí Climent <marticlilop@gmail.com>
2024-02-28 14:44:35 +0100

    update get_contributors script

--- src/wingetui/Core/Data/Contributors.cs
+++ src/wingetui/Core/Data/Contributors.cs
@@ -1,32 +1,12 @@
-﻿namespace ModernWindow.Core.Data
+﻿using System.IO;
+using System.Linq;
+
+namespace ModernWindow.Core.Data
 {
     public static class ContributorsData
     {
-        public static string[] Contributors = new string[] {
-            "marticliment",
-            "ppvnf",
-            "panther7",
-            "RavenMacDaddy",
-            "Mikey1993",
-            "adripo",
-            "raghavdhingra24",
-            "MisterEvans78",
-            "rumplin",
-            "uKER",
-            "sklart",
-            "aaronliu0130",
-            "vedantmgoyal2009",
-            "adfnekc",
-            "BastianKamp",
-            "candrapersada",
-            "elliot-100",
-            "sitiom",
-            "joguarino",
-            "neoOpus",
-            "Carterpersall",
-            "HorridModz",
-            "reima",
-            "Satanarious",
-        };
+        public static string[] Contributors = File.ReadAllLines(
+            Path.Join(CoreData.WingetUIExecutableDirectory, "Assets", "Languages", "Contributors.list")
+        ).Where(x => x != "").ToArray();
     }
 }
