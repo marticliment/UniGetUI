08067f52227ef35e29b199d3a665cac55b68b8ad
Martí Climent <marticlilop@gmail.com>
2024-03-02 17:37:44 +0100

    Fix typos

--- scripts/translation_utils.py
+++ scripts/translation_utils.py
@@ -22,9 +22,10 @@ def remove_special_chars(string):
 
 
 def get_all_strings():
-    regex = r'(?<=Translate\(["\']).+?(?=["\']\))'
     translation_strings: list[str] = []
 
+    # Find c# translation strings
+    regex = r'(?<=Translate\(["\']).+?(?=["\']\))'
     for (dirpath, _dirnames, filenames) in os.walk(".", topdown=True):
         for file in filenames:
             _file_name, file_ext = os.path.splitext(file)
@@ -34,6 +35,18 @@ def get_all_strings():
                 matches: list[str] = re.findall(regex, f.read())
                 for match in matches:
                     translation_strings.append(match.encode('raw_unicode_escape').decode('unicode_escape'))
+
+    # Find XAML translation strings
+    regex = r'<[a-zA-Z]+:TranslatedTextBlock[ a-zA-Z0-9=\"\']+Text=["\'].+["\'][ a-zA-Z0-9=\"\']*\/?>'
+    for (dirpath, _dirnames, filenames) in os.walk(".", topdown=True):
+        for file in filenames:
+            _file_name, file_ext = os.path.splitext(file)
+            if (file_ext != ".xaml"):
+                continue
+            with open(os.path.join(dirpath, file), "r", encoding="utf-8") as f:
+                matches: list[str] = re.findall(regex, f.read())
+                for match in matches:
+                    translation_strings.append(match.split(" Text=\"")[1].split("\"")[0].encode('raw_unicode_escape').decode('unicode_escape'))
 
     translation_strings = list(set(translation_strings))  # uniq
     translation_strings.sort(key=lambda x: (remove_special_chars(x.lower()), x))
