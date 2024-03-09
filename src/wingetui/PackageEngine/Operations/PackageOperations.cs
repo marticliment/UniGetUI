941bfd9e5946f4d3bd7d8af2617695fb2d444727
Martí Climent <marticlilop@gmail.com>
2024-03-09 20:20:39 +0100

    fix "uninstall  as administrator" installing the package (fix #1869)

--- src/wingetui/PackageEngine/Operations/PackageOperations.cs
+++ src/wingetui/PackageEngine/Operations/PackageOperations.cs
@@ -395,7 +395,7 @@ namespace ModernWindow.PackageEngine.Operations
                     p.WaitForExit();
                 }
                 startInfo.FileName = CoreData.GSudoPath;
-                startInfo.Arguments = "\"" + Package.Manager.Status.ExecutablePath + "\"" + Package.Manager.Properties.ExecutableCallArgs + " " + string.Join(" ", Package.Manager.GetInstallParameters(Package, Options));
+                startInfo.Arguments = "\"" + Package.Manager.Status.ExecutablePath + "\"" + Package.Manager.Properties.ExecutableCallArgs + " " + string.Join(" ", Package.Manager.GetUninstallParameters(Package, Options));
             }
             else
             {
