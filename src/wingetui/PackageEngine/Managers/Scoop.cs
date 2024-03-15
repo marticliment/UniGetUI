8805cf9af9a5780015c857ea9ab26bd3825f8ea0
Martí Climent <marticlilop@gmail.com>
2024-03-15 23:26:15 +0100

    Prevent AM/PM strings from messing scoop source loader (fix #1905)

--- src/wingetui/PackageEngine/Managers/Scoop.cs
+++ src/wingetui/PackageEngine/Managers/Scoop.cs
@@ -393,7 +393,7 @@ public class Scoop : PackageManagerWithSources
                     }
                     else if (line.Trim() != "")
                     {
-                        string[] elements = Regex.Replace(line.Trim(), " {2,}", " ").Split(' ');
+                        string[] elements = Regex.Replace(line.Replace("AM", "").Replace("PM", "").Trim(), " {2,}", " ").Split(' ');
                         if (elements.Length >= 5)
                         {
                             if (!elements[1].Contains("https://"))
