af575539d26c2698561d22b20710a1279210b893
Martí Climent <marticlilop@gmail.com>
2024-04-01 00:07:45 +0200

    Undo breaking commit

--- src/UniGetUI/bin/x64/Debug/net8.0-windows10.0.19041.0/choco-cli/extensions/chocolatey-windowsupdate/chocolatey-windowsupdate.psm1
+++
@@ -1,9 +0,0 @@
-$scriptRoot = Split-Path -Path $MyInvocation.MyCommand.Definition
-
-$publicFunctions = @(
-    'Test-WindowsUpdate',
-    'Install-WindowsUpdate'
-)
-
-Get-ChildItem -Path "$scriptRoot\*.ps1" | ForEach-Object { . $_ }
-Export-ModuleMember -Function $publicFunctions
