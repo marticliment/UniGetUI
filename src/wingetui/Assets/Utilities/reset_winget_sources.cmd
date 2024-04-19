155ee4e700926a7423dd5fccfaa9791379a31f01
pomodori92 <45762339+pomodori92@users.noreply.github.com>
2024-04-19 16:08:23 +0200

    Fix reset_winget_sources.cmd
    Fix: when a Microsoft Windows limited user executes it and the script arrives at the line 11:
    powershell -file "%~dp0\delete_winget_databases.ps1"
    
    it got this error:
    Failed to load file C:\Program Files\WingetUI\Assets\Utilities\delete_winget_databases.ps1. The file
    C:\Program Files\WingetUI\Assets\Utilities\delete_winget_databases.ps1 s not digitally signed. The script can't
    run on the current system. For more information about running scripts and setting policies
    execution policies, see about_Execution_Policies at https://go.microsoft.com/fwlink/?LinkID=135170.
        + CategoryInfo          : Protection error: (:) [], ParentContainsErrorRecordException
        + FullyQualifiedErrorId : UnauthorizedAccess
    
    Adding the following line before line 11 solves the error:
    powershell Set-ExecutionPolicy Bypass

--- src/wingetui/Assets/Utilities/reset_winget_sources.cmd
+++ src/wingetui/Assets/Utilities/reset_winget_sources.cmd
@@ -8,7 +8,9 @@ echo This script will reset winget sources.
 echo . Please wait... 
 echo winget path: %wingetpath%
 echo Deleting Winget local data sources...
+powershell Set-ExecutionPolicy Unrestricted
 powershell -file "%~dp0\delete_winget_databases.ps1"
+powershell Set-ExecutionPolicy Default
 echo Performing reset...
 cd %dirpathx86%
 %sudopath% .\winget.exe source remove winget
