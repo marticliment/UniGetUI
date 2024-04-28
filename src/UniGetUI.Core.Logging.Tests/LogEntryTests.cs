ecfaccbd7dcc38e0b8d5a28a50e5ec8e5e666030
Martí Climent <marticlilop@gmail.com>
2024-04-28 23:01:00 +0200

    Update LogEntryTests.cs

--- src/UniGetUI.Core.Logging.Tests/LogEntryTests.cs
+++ src/UniGetUI.Core.Logging.Tests/LogEntryTests.cs
@@ -3,12 +3,19 @@ namespace UniGetUI.Core.Logging.Tests
     public class LogEntryTests
     {
         [Fact]
-        public void TestLogEntry()
+        public async Task TestLogEntry()
         {
             var startTime = DateTime.Now;
+
+            await Task.Delay(100);
+
             var logEntry1 = new LogEntry("Hello World", LogEntry.SeverityLevel.Info);
+            await Task.Delay(50);
             var logEntry2 = new LogEntry("Hello World 2", LogEntry.SeverityLevel.Debug);
+            await Task.Delay(50);
             var logEntry3 = new LogEntry("Hello World 3", LogEntry.SeverityLevel.Error);
+
+            await Task.Delay(100);
 
             var endTime = DateTime.Now;
 
@@ -23,6 +30,8 @@ namespace UniGetUI.Core.Logging.Tests
             Assert.True(logEntry1.Time > startTime && logEntry1.Time < endTime);
             Assert.True(logEntry2.Time > startTime && logEntry2.Time < endTime);
             Assert.True(logEntry3.Time > startTime && logEntry3.Time < endTime);
+            Assert.True(logEntry1.Time < logEntry2.Time);
+            Assert.True(logEntry2.Time < logEntry3.Time);
         }
     }
 }
