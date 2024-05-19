65806754e9f05b986428ea29f642fc793c9afac3
Martí Climent <marticlilop@gmail.com>
2024-05-19 17:10:41 +0200

    Improvements to build script

--- build_release.bat
+++ build_release.bat
@@ -11,20 +11,25 @@ popd ..
 
 rem clean old builds
 taskkill /im wingetui.exe /f
-rmdir /Q /S src\UniGetUI\obj
-rmdir /Q /S src\UniGetUI\bin
 
 rem build executable
-pushd src 
-nuget restore
-popd ..
-"C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin\amd64\MSBuild.exe" src/UniGetUI/UniGetUI.csproj /noLogo /property:Configuration=Release /property:Platform=x64
+dotnet clean src/UniGetUI.sln
+dotnet publish src/UniGetUI/UniGetUI.csproj /noLogo /property:Configuration=Release /property:Platform=x64
 
 rem sign code
-pushd src\UniGetUI\bin\x64\Release\net8.0-windows10.0.19041.0\win-x64
-"Y:\- Signing\signtool-x64\signtool.exe" sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "Y:\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "Y:\- Signing\metadata.json" "wingetui.exe" "wingetui.dll"
-popd
+pushd src\UniGetUI\bin\x64\Release\net8.0-windows10.0.19041.0\win-x64\publish
+echo .
+echo .
+echo You may want to sign now the following executables
+cd
+echo WingetUI.dll
+echo WingetUI.exe
+echo .
+echo .
 pause
+cp WingetUI.exe UniGetUI.exe
+popd
+
 
 set INSTALLATOR="%SYSTEMDRIVE%\Program Files (x86)\Inno Setup 6\ISCC.exe"
 if exist %INSTALLATOR% (
