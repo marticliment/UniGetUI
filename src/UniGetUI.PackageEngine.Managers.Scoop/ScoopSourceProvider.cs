d683128042c6c2720f64e24fd19055f180826065
Martí Climent <marticlilop@gmail.com>
2024-05-29 22:29:27 +0200

    Fix sources not showing on Scoop when am/pm lowercase format is used (fix #2070)

--- src/UniGetUI.PackageEngine.Managers.Scoop/ScoopSourceProvider.cs
+++ src/UniGetUI.PackageEngine.Managers.Scoop/ScoopSourceProvider.cs
@@ -71,7 +71,7 @@ namespace UniGetUI.PackageEngine.Managers.ScoopManager
                         }
                         else if (line.Trim() != "")
                         {
-                            string[] elements = Regex.Replace(line.Replace("AM", "").Replace("PM", "").Trim(), " {2,}", " ").Split(' ');
+                            string[] elements = Regex.Replace(line.Replace("AM", "").Replace("am", "").Replace("PM", "").Replace("pm", "").Trim(), " {2,}", " ").Split(' ');
                             if (elements.Length >= 5)
                             {
                                 if (!elements[1].Contains("https://"))
