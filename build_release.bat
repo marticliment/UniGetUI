f3f87cd86e4b9974b22674f021977ea7eaa284dc
Martí Climent <marticlilop@gmail.com>
2024-07-28 16:35:21 +0200

    update build script

--- build_release.bat
+++ build_release.bat
@@ -35,8 +35,8 @@ mkdir unigetui_bin
 robocopy src\UniGetUI\bin\x64\Release\net8.0-windows10.0.19041.0\win-x64\publish unigetui_bin *.* /MOVE /E
 rem pushd src\UniGetUI\bin\x64\Release\net8.0-windows10.0.19041.0\win-x64\publish
 pushd unigetui_bin
-signtool.exe sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "C:\SomePrograms\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "C:\SomePrograms\- Signing\metadata.json" UniGetUI.exe
-signtool.exe sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "C:\SomePrograms\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "C:\SomePrograms\- Signing\metadata.json" UniGetUI.dll
+signtool.exe sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "Y:\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "Y:\- Signing\metadata.json" UniGetUI.exe
+signtool.exe sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "Y:\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "Y:\- Signing\metadata.json" UniGetUI.dll
 echo .
 echo .
 echo You may want to sign now the following executables
@@ -54,7 +54,7 @@ set INSTALLATOR="%SYSTEMDRIVE%\Program Files (x86)\Inno Setup 6\ISCC.exe"
 if exist %INSTALLATOR% (
     %INSTALLATOR% "UniGetUI.iss"
     echo You may now sign the installer
-    "Y:\- Signing\signtool-x64\signtool.exe" sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "Y:\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "Y:\- Signing\metadata.json" "UniGetUI Installer.exe"
+    signtool.exe sign /v /debug /fd SHA256 /tr "http://timestamp.acs.microsoft.com" /td SHA256 /dlib "Y:\- Signing\azure.codesigning.client\x64\Azure.CodeSigning.Dlib.dll" /dmdf "Y:\- Signing\metadata.json" "UniGetUI Installer.exe"
     del "WingetUI Installer.exe"
     copy "UniGetUI Installer.exe" "WingetUI Installer.exe" 
     pause
