0d48b300578080af45c6f3350800ffb3f8487813
Max <mjrixnertx@gmail.com>
2024-10-15 02:24:26 -0500

    Add vcpkg to the list of aware-of package managers

--- src/UniGetUI.PackageEngine.Managers.Vcpkg/Vcpkg.cs
+++ src/UniGetUI.PackageEngine.Managers.Vcpkg/Vcpkg.cs
@@ -14,7 +14,30 @@ namespace UniGetUI.PackageEngine.Managers.VcpkgManager {
     {
 		public Vcpkg()
 		{
-			
+			Capabilities = new ManagerCapabilities
+			{
+				CanRunAsAdmin = false, // TODO: check this; should this be true since we need admin to install to protected directories?
+				SupportsCustomSources = true, // TODO: check this; are different triplets "different sources"?
+			};
+
+			Properties = new ManagerProperties
+			{
+				Name = "vcpkg",
+				Description = CoreTools.Translate("A popular C/C++ library manager. Full of C/C++ libraries and other C/C++-related utilities<br>Contains: <b>C/C++ libraries and related utilities</b>"),
+				// TODO: IconID
+				// TODO: ColorIconID
+				ExecutableFriendlyName = "vcpkg",
+				InstallVerb = "install",
+				UninstallVerb = "remove",
+				UpdateVerb = "update",
+				ExecutableCallArgs = "",
+				// TODO: Sources
+				DefaultSource = new ManagerSource(this, "Windows x64", new Uri("https://vcpkg.io/")),
+                KnownSources = [new ManagerSource(this, "Windows x64", new Uri("https://vcpkg.io/"))],
+			};
+
+			SourceProvider = new VcpkgSourceProvider(this);
+			OperationProvider = new VcpkgOperationProvider(this);
 		}
 
         protected override IEnumerable<Package> FindPackages_UnSafe(string query)
