fb373a9d03e6e804ec41ad935a7e8c11cb453d94
Martí Climent <marticlilop@gmail.com>
2024-12-26 01:38:41 +0100

    Remove commented code

--- src/UniGetUI/Controls/OperationWidgets/OperationControl.xaml.cs
+++ src/UniGetUI/Controls/OperationWidgets/OperationControl.xaml.cs
@@ -27,89 +27,13 @@ namespace UniGetUI.Interface
 
     /*
 
-    public AbstractOperation(bool IgnoreParallelInstalls = false)
-    {
-        Status = OperationStatus.InQueue;
-
-        ActionButton.Click += ActionButtonClicked;
-        OutputViewewBlock.Click += (_, _) =>
-        {
-            OpenLiveViewDialog();
-        };
-    }
-
-    public async void OpenLiveViewDialog()
-    {
-        OutputDialog.XamlRoot = XamlRoot;
-        LiveOutputTextBlock.Blocks.Clear();
-        Paragraph p = new()
-        {
-            LineHeight = 4.8
-        };
-        foreach (OutputLine line in ProcessOutput)
-        {
-            if (line.Type is OutputLine.LineType.STDOUT)
-                p.Inlines.Add(new Run { Text = line.Contents + "\x0a" });
-            else if (line.Type is OutputLine.LineType.Header)
-                // TODO: Theme-aware colors
-                p.Inlines.Add(new Run { Text = line.Contents + "\x0a", Foreground = new SolidColorBrush(Colors.Azure)});
-            else
-                p.Inlines.Add(new Run { Text = line.Contents + "\x0a", Foreground = new SolidColorBrush(Colors.Red)});
-        }
-        LiveOutputTextBlock.Blocks.Add(p);
-        IsDialogOpen = true;
-
-        if (await MainApp.Instance.MainWindow.ShowDialogAsync(OutputDialog) == ContentDialogResult.Secondary)
-        {
-            LiveOutputScrollBar.ScrollToVerticalOffset(LiveOutputScrollBar.ScrollableHeight);
-            WindowsClipboard.SetText(string.Join('\n', ProcessOutput.ToArray()));
-        }
-        IsDialogOpen = false;
-    }
-
-
-
-
     protected async Task MainThread()
     {
         try
         {
 
-            if (Status is OperationStatus.Canceled)
-            {
-                return; // If the operation was canceled, do nothing.
-            }
-
             MainApp.Instance.TooltipStatus.OperationsInProgress += 1;
 
-            LineInfoText = CoreTools.Translate("Launching subprocess...");
-            ProcessStartInfo startInfo = new()
-            {
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                StandardOutputEncoding = System.Text.Encoding.UTF8,
-                StandardInputEncoding = System.Text.Encoding.UTF8,
-                StandardErrorEncoding = System.Text.Encoding.UTF8,
-                WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
-            };
-
-            Process = new Process();
-
-            Process.StartInfo = await BuildProcessInstance(startInfo);
-            // Process.StartInfo = CoreTools.UpdateEnvironmentVariables(Process.StartInfo);
-
-            foreach (string infoLine in GenerateProcessLogHeader())
-            {
-                ProcessOutput.Add(new(infoLine, OutputLine.LineType.Header));
-            }
-
-            ProcessOutput.Add(new("Process Executable     : " + Process.StartInfo.FileName, OutputLine.LineType.Header));
-            ProcessOutput.Add(new("Process Call Arguments : " + Process.StartInfo.Arguments, OutputLine.LineType.Header));
-            ProcessOutput.Add(new("Working Directory      : " + Process.StartInfo.WorkingDirectory, OutputLine.LineType.Header));
-            ProcessOutput.Add(new("Process Start Time     : " + DateTime.Now, OutputLine.LineType.Header));
 
             Process.OutputDataReceived += (_, e) => DispatcherQueue.TryEnqueue(async () =>
             {
@@ -129,54 +53,11 @@ namespace UniGetUI.Interface
                 }
             });
 
-            Process.ErrorDataReceived += (_, e) => DispatcherQueue.TryEnqueue(async () =>
-            {
-                if (e.Data?.Trim() is string line && line != String.Empty)
-                {
-                    if (Status is not OperationStatus.Canceled)
-                    {
-                        LineInfoText = line;
-                        if(line.Length > 3) ProcessOutput.Add(new(line, OutputLine.LineType.STDERR));
-                    }
-                }
-            });
-
-
-            Process.Start();
-            PostProcessStartAction();
-
-            Process.BeginOutputReadLine();
-            Process.BeginErrorReadLine();
-
-            Status = OperationStatus.Running;
-
-            await Process.WaitForExitAsync();
-            PostProcessEndAction();
-
-            ProcessOutput.Add(new("Process Exit Code      : " + Process.ExitCode, OutputLine.LineType.Header));
-            ProcessOutput.Add(new("Process End Time       : " + DateTime.Now, OutputLine.LineType.Header));
-
-            AfterFinshAction postAction = AfterFinshAction.ManualClose;
-
-            OperationVeredict OperationVeredict = await GetProcessVeredict(Process.ExitCode, RawProcessOutput);
-
 
             if (Status is not OperationStatus.Canceled)
             {
                 switch (OperationVeredict)
                 {
-                    case OperationVeredict.Success or OperationVeredict.RestartRequired:
-                        Status = OperationStatus.Succeeded;
-                        postAction = await HandleSuccess();
-                        RemoveFromQueue();
-                        break;
-
-                    case OperationVeredict.Canceled:
-                        Status = OperationStatus.Canceled;
-                        RemoveFromQueue();
-                        postAction = AfterFinshAction.ManualClose;
-                        await HandleCancelation();
-                        break;
 
                     case OperationVeredict.AutoRetry:
                         Status = OperationStatus.InQueue;
@@ -190,9 +71,6 @@ namespace UniGetUI.Interface
                         postAction = await HandleFailure();
                         MainApp.Instance.TooltipStatus.ErrorsOccurred -= 1;
                         break;
-
-                    default:
-                        throw new ArgumentException($"Unexpected OperationVeredict {OperationVeredict}");
                 }
             }
 
@@ -276,15 +154,5 @@ namespace UniGetUI.Interface
         RemoveFromQueue();
         PEInterface.OperationList.Remove(this);
     }
-
-    protected void Retry()
-    {
-        AddToQueue_Priority();
-        LineInfoText = CoreTools.Translate("Retrying, please wait...");
-        ProcessOutput.Clear();
-        Status = OperationStatus.InQueue;
-        _ = MainThread();
-    }
-
 }*/
 }
