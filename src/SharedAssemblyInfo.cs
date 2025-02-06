ffe16e9f6eaf5d5ed64fcf42b6ebc9a977503a95
Martí Climent <marticlilop@gmail.com>
2025-02-06 13:37:34 +0100

    Update language files

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCopyright("2025, Martí Climent")]
 [assembly: AssemblyVersion("3.1.6.0")]
 [assembly: AssemblyFileVersion("3.1.6.0")]
-[assembly: AssemblyInformationalVersion("3.1.6-beta4")]
+[assembly: AssemblyInformationalVersion("3.1.6")]
 [assembly: SupportedOSPlatform("windows10.0.19041")]
