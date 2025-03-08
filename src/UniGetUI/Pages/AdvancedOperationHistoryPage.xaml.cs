f3e8d49ae7013adc16a292e4e067a6cdf58faa7f
Max <mjrixnertx@gmail.com>
2025-03-08 11:05:12 -0600

    Add an 'advanced operation history' menu item

---
+++ src/UniGetUI/Pages/AdvancedOperationHistoryPage.xaml.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using ExternalLibraries.Clipboard;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using UniGetUI.Core.Tools;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace UniGetUI.Interface.Pages
+{
+    public partial class AdvancedOperationHistoryPage : IKeyboardShortcutListener, IEnterLeaveListener
+    {
+
+
+        public void ReloadTriggered()
+        { }
+        public void SelectAllTriggered()
+        { }
+        public void SearchTriggered()
+        { }
+        public void OnEnter()
+        { }
+        public void OnLeave()
+        { }
+    }
+}
