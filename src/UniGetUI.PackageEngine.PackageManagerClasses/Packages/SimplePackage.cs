39721dd850a64c33bbb7dc990c0d100d1e6fe9c2
Max <mjrixnertx@gmail.com>
2025-03-10 17:32:03 -0500

    Fix JSON serialization for SimplePackage and AdvancedOperationHistoryEntry

--- src/UniGetUI.PackageEngine.PackageManagerClasses/Packages/SimplePackage.cs
+++ src/UniGetUI.PackageEngine.PackageManagerClasses/Packages/SimplePackage.cs
@@ -1,35 +1,92 @@
+using System.Text.Json.Serialization;
 using UniGetUI.Interface.Enums;
 using UniGetUI.PackageEngine.Enums;
 using UniGetUI.PackageEngine.Interfaces;
 
 namespace UniGetUI.PackageEngine.PackageClasses
 {
-    public class SimplePackage(IPackage Source)
+    public class SimplePackage
     {
-        public string Description { get; } = "Not loaded";
-        public string[] Tags { get; } = [];
-        public string Name { get; } = Source.Name;
-        public string Id { get; } = Source.Id;
-        public string VersionString { get; } = Source.VersionString;
-        public string NewVersionString { get; } = Source.NewVersionString;
-        public string IconId { get; } = Package.GetPackageIconId(Source.Id, Source.Manager.Name, Source.Source.Name);
-        public string ManagerName { get; } = Source.Manager.Name;
-        public string ManagerDisplayName { get; } = Source.Manager.DisplayName;
-        public IconType ManagerIconId { get; } = Source.Manager.Properties.IconId;
-        public string SourceName { get; } = Source.Source.Name;
-        public Uri SourceUrl { get; } = Source.Source.Url;
-        public IconType SourceIconId { get; } = Source.Source.IconId;
+        public SimplePackage(IPackage source)
+        {
+            Description = "Not loaded";
+            Tags = [];
+            Name = source.Name;
+            Id = source.Id;
+            VersionString = source.VersionString;
+            NewVersionString = source.NewVersionString;
+            IconId = Package.GetPackageIconId(source.Id, source.Manager.Name, source.Source.Name);
+            ManagerName = source.Manager.Name;
+            ManagerDisplayName = source.Manager.DisplayName;
+            ManagerIconId = source.Manager.Properties.IconId;
+            SourceName = source.Source.Name;
+            SourceUrl = source.Source.Url;
+            SourceIconId = source.Source.IconId;
+        }
+
+        [JsonConstructor]
+        public SimplePackage(
+            string description, string[] tags, string name, string id,
+            string versionString, string newVersionString, string iconId,
+            string managerName, string managerDisplayName, IconType managerIconId,
+            string sourceName, Uri sourceUrl, IconType sourceIconId)
+        {
+            Description = description;
+            Tags = tags;
+            Name = name;
+            Id = id;
+            VersionString = versionString;
+            NewVersionString = newVersionString;
+            IconId = iconId;
+            ManagerName = managerName;
+            ManagerDisplayName = managerDisplayName;
+            ManagerIconId = managerIconId;
+            SourceName = sourceName;
+            SourceUrl = sourceUrl;
+            SourceIconId = sourceIconId;
+        }
+
+        public string Description { get; }
+        public string[] Tags { get; }
+        public string Name { get; }
+        public string Id { get; }
+        public string VersionString { get; }
+        public string NewVersionString { get; }
+        public string IconId { get; }
+        public string ManagerName { get; }
+        public string ManagerDisplayName { get; }
+        public IconType ManagerIconId { get; }
+        public string SourceName { get; }
+        public Uri SourceUrl { get; }
+        public IconType SourceIconId { get; }
     }
 
-    public class AdvancedOperationHistoryEntry(
-        IPackage Source,
-        OperationType Type,
-        OperationStatus Status,
-        string Logs
-    ) : SimplePackage(Source)
+    public class AdvancedOperationHistoryEntry : SimplePackage
     {
-        public OperationType Type { get; } = Type;
-        public OperationStatus Status { get; } = Status;
-        public string Logs { get; } = Logs;
+        public AdvancedOperationHistoryEntry(IPackage source, OperationType type, OperationStatus status, string logs) : base(source)
+        {
+            Type = type;
+            Status = status;
+            Logs = logs;
+        }
+
+        [JsonConstructor]
+        public AdvancedOperationHistoryEntry(
+            OperationType type, OperationStatus status, string logs,
+            string description, string[] tags, string name, string id,
+            string versionString, string newVersionString, string iconId,
+            string managerName, string managerDisplayName, IconType managerIconId,
+            string sourceName, Uri sourceUrl, IconType sourceIconId)
+                : base(description, tags, name, id, versionString, newVersionString, iconId,
+                managerName, managerDisplayName, managerIconId, sourceName, sourceUrl, sourceIconId)
+        {
+            Type = type;
+            Status = status;
+            Logs = logs;
+        }
+
+        public OperationType Type { get; }
+        public OperationStatus Status { get; }
+        public string Logs { get; }
     };
 }
