a34540cf17fc580c701fdb15a61a78a509a8fbc0
Max <mjrixnertx@gmail.com>
2025-03-10 15:18:15 -0500

    Switch back to the AdvancedOperationHistoryEntry

--- src/UniGetUI/Pages/AdvancedOperationHistoryPage.xaml.cs
+++ src/UniGetUI/Pages/AdvancedOperationHistoryPage.xaml.cs
@@ -3,13 +3,20 @@ using Microsoft.UI.Xaml.Controls;
 using UniGetUI.Core.SettingsEngine;
 using UniGetUI.Core.Tools;
 using UniGetUI.Interface.Enums;
+using UniGetUI.PackageEngine.Enums;
+using UniGetUI.PackageEngine.Interfaces;
 using UniGetUI.PackageEngine.PackageClasses;
 
 namespace UniGetUI.Interface.Pages
 {
+    public class AdvancedOperationHistoryEntry(IPackage Source, OperationType Operation) : SimplePackage(Source)
+    {
+        public OperationType Operation { get; } = Operation;
+    };
+
     public partial class AdvancedOperationHistoryPage : IKeyboardShortcutListener, IEnterLeaveListener
     {
-        private readonly ObservableCollection<SimplePackage> Items = [];
+        private readonly ObservableCollection<AdvancedOperationHistoryEntry> Items = [];
 
         public AdvancedOperationHistoryPage()
         {
