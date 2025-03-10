e32680aab65d7ffb8deb8ed8c9bb779d2cba40c2
Max <mjrixnertx@gmail.com>
2025-03-10 11:28:32 -0500

    Fill out settings entry dropdown fields

--- src/UniGetUI/Pages/AdvancedOperationHistoryPage.xaml.cs
+++ src/UniGetUI/Pages/AdvancedOperationHistoryPage.xaml.cs
@@ -13,15 +13,15 @@ using Windows.UI;
 
 namespace UniGetUI.Interface.Pages
 {
-    public class OperationHistoryEntry
+    public class AdvancedOperationHistoryEntry
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
     }
 
     public partial class AdvancedOperationHistoryPage : IKeyboardShortcutListener, IEnterLeaveListener
     {
-        private ObservableCollection<OperationHistoryEntry> Items = new ();
+        private ObservableCollection<AdvancedOperationHistoryEntry> Items = new ();
 
         public AdvancedOperationHistoryPage()
         {
@@ -34,11 +34,11 @@ namespace UniGetUI.Interface.Pages
         {
             Items.Clear();
 
-            Items.Add(new OperationHistoryEntry { Title = "Test 1", Content = "Content 1" });
-            Items.Add(new OperationHistoryEntry { Title = "Test 2", Content = "Content 2" });
-            Items.Add(new OperationHistoryEntry { Title = "Test 3", Content = "Content 3" });
-            Items.Add(new OperationHistoryEntry { Title = "Test 4", Content = "Content 4" });
-            Items.Add(new OperationHistoryEntry { Title = "Test 5", Content = "Content 5" });
+            Items.Add(new AdvancedOperationHistoryEntry { Title = "Test 1", Content = "Content 1" });
+            Items.Add(new AdvancedOperationHistoryEntry { Title = "Test 2", Content = "Content 2" });
+            Items.Add(new AdvancedOperationHistoryEntry { Title = "Test 3", Content = "Content 3" });
+            Items.Add(new AdvancedOperationHistoryEntry { Title = "Test 4", Content = "Content 4" });
+            Items.Add(new AdvancedOperationHistoryEntry { Title = "Test 5", Content = "Content 5" });
         }
 
         public void ReloadTriggered()
