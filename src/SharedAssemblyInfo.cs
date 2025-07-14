c9dc95e7039a691ab52a9efdb19d0d4c34bf6713
Martí Climent <marticlilop@gmail.com>
2025-07-14 16:19:20 +0200

    update version number

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCopyright("2025, Martí Climent")]
 [assembly: AssemblyVersion("3.2.1.0")]
 [assembly: AssemblyFileVersion("3.2.1.0")]
-[assembly: AssemblyInformationalVersion("3.2.1-beta4")]
+[assembly: AssemblyInformationalVersion("3.2.1-beta5")]
 [assembly: SupportedOSPlatform("windows10.0.19041")]
