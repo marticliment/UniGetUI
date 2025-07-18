af630344264f932f3837afc3c77dd50a7bf096b6
Martí Climent <marticlilop@gmail.com>
2025-07-18 23:45:19 +0200

    Automatically guess WinGet source type (fix #3508)

--- src/UniGetUI.PackageEngine.Managers.WinGet/Helpers/WinGetSourceHelper.cs
+++ src/UniGetUI.PackageEngine.Managers.WinGet/Helpers/WinGetSourceHelper.cs
@@ -6,14 +6,23 @@ namespace UniGetUI.PackageEngine.Managers.WingetManager
 {
     internal sealed class WinGetSourceHelper : BaseSourceHelper
     {
+        private readonly string[][] _sourceTypes =
+        [
+            ["--type", "Microsoft.PreIndexed.Package"],
+            ["--type", "Microsoft.Rest"],
+        ];
+        private Dictionary<string, int> _attemptedSourceTypes = new();
+
         public WinGetSourceHelper(WinGet manager) : base(manager) { }
 
         public override string[] GetAddSourceParameters(IManagerSource source)
         {
             List<string> args = ["source", "add", "--name", source.Name, "--arg", source.Url.ToString(), "--accept-source-agreements", "--disable-interactivity"];
+
             if (source.Name != "winget")
             {
-                args.AddRange(["--type", "Microsoft.Rest"]);
+                int sourceIndex = _attemptedSourceTypes.GetValueOrDefault(source.Name);
+                args.AddRange(_sourceTypes[sourceIndex]);
             }
 
             return args.ToArray();
@@ -26,6 +35,19 @@ namespace UniGetUI.PackageEngine.Managers.WingetManager
 
         protected override OperationVeredict _getAddSourceOperationVeredict(IManagerSource source, int ReturnCode, string[] Output)
         {
+            if ((uint)ReturnCode is 0x8A150045 or 0x801901F4)
+            {
+                int sourceIndex = _attemptedSourceTypes.GetValueOrDefault(source.Name);
+                if (sourceIndex + 1 >= _sourceTypes.Length)
+                {
+                    _attemptedSourceTypes[source.Name] = 0;
+                    return OperationVeredict.Failure;
+                }
+
+                _attemptedSourceTypes[source.Name] = sourceIndex + 1;
+                return OperationVeredict.AutoRetry;
+            }
+
             return ReturnCode == 0 ? OperationVeredict.Success : OperationVeredict.Failure;
         }
 
