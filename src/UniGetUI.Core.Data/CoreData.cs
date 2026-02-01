c77c406710a70707edc6b1ea4a1effb1b12531f3
Martí Climent <marticlilop@gmail.com>
2026-02-01 10:51:11 +0100

    update version number

--- src/UniGetUI.Core.Data/CoreData.cs
+++ src/UniGetUI.Core.Data/CoreData.cs
@@ -7,8 +7,8 @@ namespace UniGetUI.Core.Data
     {
         private static int? __code_page;
         public static int CODE_PAGE { get => __code_page ??= GetCodePage(); }
-        public const string VersionName = "3.3.6"; // Do not modify this line, use file scripts/apply_versions.py
-        public const int BuildNumber = 104; // Do not modify this line, use file scripts/apply_versions.py
+        public const string VersionName = "3.3.7-beta1"; // Do not modify this line, use file scripts/apply_versions.py
+        public const int BuildNumber = 105; // Do not modify this line, use file scripts/apply_versions.py
 
         public const string UserAgentString = $"UniGetUI/{VersionName} (https://marticliment.com/unigetui/; contact@marticliment.com)";
 
